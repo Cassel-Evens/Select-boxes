c3be5918d89ffae4bc2b34af2d80a84b289e3b29
Cassel-Evens <casselke@gmail.com>
2021-11-02 19:39:42 -0700

    add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  const beverage = $("beverage").val();
+  const beverageNumber = $parseInt(beverage);
+  const flavor = $("input:radio[name=flavor]:checked").val();
+  const dob = $("#born").val();
+  const favoriteColor= $("#color").val();
+});
